3d96696572221b947ce010b57f3cb9895eddbafb
Jung Myung Sung (정명성) <kkforgg@naver.com>
2015-12-02 16:06:12 +0900

    angularjs  ui-router 를 이용한 예제

---
+++ src/main/resources/static/js/router/ui-router.js
@@ -0,0 +1,43 @@
+myApp
+	.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+	
+	// html5 모드
+  $locationProvider.html5Mode(true).hashPrefix('!');
+  $urlRouterProvider.otherwise("/");
+  
+  $stateProvider
+	// index 라는 애는 url이 없고 view template 영역 중 2개를 변경한다..
+    .state('home', {
+				        url: "/home",
+					  	templateUrl : '/myhome/home.html',
+						controller: 'homeDemoCtrl'
+    				}
+
+      )
+	// template 는 HTML String 
+	// templateUrl 은 file Url      
+     // 게시판
+   .state('board', {
+					   	//abstract: true,
+					    url : "/board",
+						templateUrl : '/myhome/board.html',
+					    controller : 'dataListCtrl'	
+	    			
+	           
+    })
+    // 상세보기
+    .state('board.detail',{
+				    	url : "^/board/:id",
+				    	views : {
+				    		'@' : {
+				    			templateUrl : '/myhome/board.detail.html',
+				    			controller : 'dataDetailCtrl'
+				    			
+				    		}
+				    	}
+						
+	
+    })      
+
+});
+
