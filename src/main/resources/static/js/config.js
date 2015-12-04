dad8eb8f0fb4dade4496ffdd1aa145d11f7d5f70
audtjddld <audtjddld@daihan-biomedical.com>
2015-12-04 11:39:03 +0900

    페이징 부분까지 수정 완료

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -9,15 +9,3 @@ var pagingInfo = {
 		pagePerCnt : 10
 
 	}
- 
-
-angular.module("template/pagination/pagination2.html", []).run(["$templateCache", function($templateCache) {
-	  $templateCache.put("template/pagination/pagination.html",
-	    "<ul class=\"pagination\">\n" +
-	    "  <li ng-if=\"boundaryLinks\" ng-class=\"{disabled: noPrevious()}\"><a href=\"javascript:\" ng-click=\"selectPage(1, $event)\">{{getText('first')}}</a></li>\n" +
-	    "  <li ng-if=\"directionLinks\" ng-class=\"{disabled: noPrevious()}\"><a href=\"javascript:\" ng-click=\"selectPage(page - 1, $event)\">{{getText('previous')}}</a></li>\n" +
-	    "  <li ng-repeat=\"page in pages track by $index\" ng-class=\"{active: page.active}\"><a href=\"javascript:\" ng-click=\"selectPage(page.number, $event)\">{{page.text}}</a></li>\n" +
-	    "  <li ng-if=\"directionLinks\" ng-class=\"{disabled: noNext()}\"><a href=\"javascript:\" ng-click=\"selectPage(page + 1, $event)\">{{getText('next')}}</a></li>\n" +
-	    "  <li ng-if=\"boundaryLinks\" ng-class=\"{disabled: noNext()}\"><a href=\"javascript:\" ng-click=\"selectPage(totalPages, $event)\">{{getText('last')}}</a></li>\n" +
-	    "</ul>");
-	}]);
